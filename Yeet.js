b244aaff41ffeb828ac901b26d69b3d6cbc1167d
Bradley1111 <48701517+Bradley1111@users.noreply.github.com>
2019-03-20 17:55:07 -0500

    Update Yeet.js

--- Yeet.js
+++ Yeet.js
@@ -14,7 +14,7 @@ function draw() {
   fill(255,255,255)
 	rect(x,y,50,50)
 	background(0,100,0);
-	 text('unmute',100,100)	
+	 text('undeafean',100,100)	
 	text('me',150,100)
 	fill(255,224,189)
 	ellipse(x +27,y -20,40,40)//head
