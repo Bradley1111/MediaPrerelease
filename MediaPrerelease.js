16cf3a06849887d19ac18920c6f5b3eb5ad3afe6
Bradley1111 <48701517+Bradley1111@users.noreply.github.com>
2019-06-22 19:22:33 -0500

    Add files via upload

---
+++ MediaPrerelease.js
@@ -0,0 +1,22 @@
+
+let test;
+
+
+function preload() {
+test = loadImage('flower.jpg')
+
+
+}
+
+function setup() {
+createCanvas(600,600);
+
+
+
+
+}
+
+function draw() {
+fill(10,45,6)
+image(test,100,100)
+}
