c2b02559d8db235aa2d019615670b3e1fb7ee000
Bradley1111 <48701517+Bradley1111@users.noreply.github.com>
2019-06-22 23:26:22 -0500

    Add files via upload

---
+++ MediaPrerelease.js
@@ -0,0 +1,47 @@
+
+let map;
+wipeprogress = 650
+kpc = 0
+winstate = 0
+function preload() {
+
+map = loadImage('M.jpg')
+
+}
+
+function setup() {
+createCanvas(600,600);
+
+
+
+
+}
+
+function draw() {
+	if (winstate === 0)  {
+Image(map)
+fill(55,55,55)
+rect(90,540,400,50) // progressbarbase
+fill(50,90,50)
+rect(100,550,wipeprogress / 2,30)//progressbardisplay
+
+if (keyCode === ENTER) {wipeprogress = wipeprogress + 0.5
+kpc = kpc + 10}//progressbar input
+
+
+
+if (kpc === 50) {keyCode = 'Q'
+kpc = 0}//progressbar inputstop timer
+
+
+if (wipeprogress === 760) {winstate = 1}
+
+
+
+
+
+}//if winstate end
+
+
+
+}//program end
